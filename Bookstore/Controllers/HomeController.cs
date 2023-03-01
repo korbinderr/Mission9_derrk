749de2630284183925bc859791f3306b33edf407
korbin-derr <korbinderr@gmail.com>
2023-03-01 15:51:37 -0700

    Add project files.

---
+++ Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        // Disjoined now.. 'abstraction'. the controller isn't calling the context anymore, doesn't even know... all it knows is that it's calling some repository
+        public HomeController(IBookstoreRepository rep)
+        {
+            repo = rep;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize =10;
+
+            var i = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                Pageinfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            //var books = repo.Books
+            //    .OrderBy(b => b.Title)
+            //    .Skip((pageNum-1) * pageSize)
+            //    .Take(pageSize);
+
+            return View(i);
+        }
+    }
+}
---
+++ Bookstore/Models/PaginationTagHelper.cs
@@ -0,0 +1,63 @@
+﻿using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    [HtmlTargetElement("div", Attributes = "page-iterate")]
+    public class PaginationTagHelper : TagHelper
+    {
+        // Dynamically create the page links for us. 
+
+        private IUrlHelperFactory uhf; 
+
+        public PaginationTagHelper (IUrlHelperFactory ii)
+        {
+            uhf = ii;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+        
+        public PageInfo PageIterate { get; set; }
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i < PageIterate.TotalPages + 1; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageIterate.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
